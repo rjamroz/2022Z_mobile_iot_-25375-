72ee9db453adc73eb7a0f1349a408bee207625db
rjamroz <67823640+rjamroz@users.noreply.github.com>
2023-01-20 18:23:40 +0100

    Add files via upload

---
+++ Androoid zadania/MainPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    string translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+                {
+                    if (PhoneDialer.Default.IsSupported)
+                        PhoneDialer.Default.Open(translatedNumber);
+                }
+                    catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                    catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+}
+/*
+public partial class TestPage : ContentPage
+{
+    int count = 0;
+
+    // Named Label - declared as a member of the class
+    Label counterLabel;
+
+    public TestPage()
+    {
+        var myScrollView = new ScrollView();
+
+        var myStackLayout = new VerticalStackLayout();
+        myScrollView.Content = myStackLayout;
+
+        counterLabel = new Label
+        {
+            Text = "Current count: 0",
+            FontSize = 18,
+            FontAttributes = FontAttributes.Bold,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        myStackLayout.Children.Add(counterLabel);
+
+        var myButton = new Button
+        {
+            Text = "Click me",
+            HorizontalOptions = LayoutOptions.Center
+        };
+        myStackLayout.Children.Add(myButton);
+
+        myButton.Clicked += OnCounterClicked;
+
+        this.Content = myScrollView;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        counterLabel.Text = $"Current count: {count}";
+
+        SemanticScreenReader.Announce(counterLabel.Text);
+    }
+}
+
+*/
---
+++ Androoid zadania/mobile_iot_lab02/Kod.cs
@@ -0,0 +1,159 @@
+﻿/* 
+ * namespace Phoneword;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    string translatedNumber;
+
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = "Call " + translatedNumber;
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+    async void OnCall(object sender, System.EventArgs e)
+    {
+        if (await this.DisplayAlert(
+            "Dial a Number",
+            "Would you like to call " + translatedNumber + "?",
+            "Yes",
+            "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                    PhoneDialer.Default.Open(translatedNumber);
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                // Other error has occurred.
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+            }
+        }
+    }
+}
+/*
+public partial class TestPage : ContentPage
+{
+    int count = 0;
+
+    // Named Label - declared as a member of the class
+    Label counterLabel;
+
+    public TestPage()
+    {
+        var myScrollView = new ScrollView();
+
+        var myStackLayout = new VerticalStackLayout();
+        myScrollView.Content = myStackLayout;
+
+        counterLabel = new Label
+        {
+            Text = "Current count: 0",
+            FontSize = 18,
+            FontAttributes = FontAttributes.Bold,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        myStackLayout.Children.Add(counterLabel);
+
+        var myButton = new Button
+        {
+            Text = "Click me",
+            HorizontalOptions = LayoutOptions.Center
+        };
+        myStackLayout.Children.Add(myButton);
+
+        myButton.Clicked += OnCounterClicked;
+
+        this.Content = myScrollView;
+    }
+
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
+        counterLabel.Text = $"Current count: {count}";
+
+        SemanticScreenReader.Announce(counterLabel.Text);
+    }
+}
+
+
+
+ XAML:
+
+
+<?xml version="1.0" encoding="utf-8" ?>
+<ContentPage xmlns="http://schemas.microsoft.com/dotnet/2021/maui"
+             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
+             x:Class="Phoneword.MainPage">
+
+    <VerticalStackLayout Spacing="15" Padding="20">
+        <Label Text = "Enter a Phoneword"
+               FontSize ="20"/>
+        <Entry x:Name = "PhoneNumberText"
+               Text = "1-555-NETMAUI" />
+        <Button x:Name = "TranslateButton"
+                Text = "Translate"
+                Clicked = "OnTranslate"/>
+        <Button x:Name = "CallButton"
+                Text = "Call"
+                IsEnabled = "False"
+                Clicked = "OnCall"/>
+    </VerticalStackLayout>
+    
+ <!--   <ScrollView>
+        <VerticalStackLayout
+            Spacing="25"
+            Padding="30,0"
+            VerticalOptions="Center">
+
+            <Image
+                Source="dotnet_bot.png"
+                SemanticProperties.Description="Cute dot net bot waving hi to you!"
+                HeightRequest="200"
+                HorizontalOptions="Center" />
+
+            <Label
+                Text="Hello, World!"
+                SemanticProperties.HeadingLevel="Level1"
+                FontSize="32"
+                HorizontalOptions="Center" />
+
+            <Label
+                Text="Welcome to .NET Multi-platform App UI"
+                SemanticProperties.HeadingLevel="Level2"
+                SemanticProperties.Description="Welcome to dot net Multi platform App U I"
+                FontSize="18"
+                HorizontalOptions="Center" />
+
+            <Button
+                x:Name="CounterBtn"
+                Text="Click me"
+                SemanticProperties.Hint="Counts the number of times you click"
+                Clicked="OnCounterClicked"
+                HorizontalOptions="Center" />
+
+        </VerticalStackLayout>
+    </ScrollView>
+-->
+</ContentPage>
+*/ 
